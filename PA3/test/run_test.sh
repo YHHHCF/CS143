44052b99364c154c4a59553d04bdaedd5797f09d
zhejin <zhejinh@myth53.stanford.edu>
2021-05-13 18:28:55 -0700

    add tests

---
+++ PA3/test/run_test.sh
@@ -0,0 +1,24 @@
+gtlexer='/usr/class/cs143/bin/lexer'
+gtparser='/usr/class/cs143/bin/parser'
+gtsemant='/usr/class/cs143/bin/semant'
+
+cd ..
+make clean;
+make semant;
+cd ./test/
+
+# Unit tests I created
+# echo ==================Unit tests===================
+
+# for file in ./unit/*_good.cl ; do
+#     echo Testing "$file"
+#     diff <($gtlexer $file | ../parser) <($gtlexer $file | $gtparser)
+# done
+
+# Copied from /usr/class/cs143/examples/
+echo =================Example tests=================
+
+for file in ./examples/* ; do
+    echo Testing "$file"
+    diff <($gtlexer $file | $gtparser | ../semant) <($gtlexer $file | $gtparser | $gtsemant)
+done
